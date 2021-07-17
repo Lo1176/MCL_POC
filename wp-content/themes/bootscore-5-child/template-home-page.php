f62e87053187430cf1018072fb9f3e3f7fba6863
Laurent Binder <laurentbinder@gmail.com>
2021-07-17 20:19:02 +0200

    accordeon read more

--- wp-content/themes/bootscore-5-child/template-home-page.php
+++ wp-content/themes/bootscore-5-child/template-home-page.php
@@ -139,15 +139,26 @@ get_header();
                 </div>
 
                 <!-- test edito -->
-                <div class="p-5 mb-4 bg-light rounded-3">
-                    <h3 id="lw-edito" class="display-1 text-center">Edito</h3>
-                    <div class="container-fluid py-5">
-                        <p class="col-md-8 fs-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint commodi est ut pariatur, voluptates, libero id maxime tenetur laudantium esse cumque recusandae error neque ratione et odit veritatis, magnam possimus.
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint commodi est ut pariatur, voluptates, libero id maxime tenetur laudantium esse cumque recusandae error neque ratione et odit veritatis, magnam possimus
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint commodi est ut pariatur, voluptates, libero id maxime tenetur laudantium esse cumque recusandae error neque ratione et odit veritatis, magnam possimus
+                <div class="container col-xl-12 col-md-8 fs-4 bg-white rounded-3">
+                    <h3 id="lw-edito" class="fs-1 text-center">Edito</h3>
+                    <div class="container text-center py-5">
+                        <p class="">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat...
                         </p>
-                        <button class="btn btn-outline-dark btn-lg" type="button">En savoir plus</button>
-
+                        <!-- read more -->
+                        <div class="accordion accordion-flush" id="accordionReadMore">
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="readMore">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                    </button>
+                                </h2>
+                            </div>
+                            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="readMore" data-bs-parent="#accordionReadMore">
+                                <div class="accordion-body">
+                                    <p class="fs-4">ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
