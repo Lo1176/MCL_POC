21ce64db31071d8d43043f2fd96786225608a3a4
Laurent Binder <laurentbinder@gmail.com>
2021-07-27 11:58:09 +0200

    add post slider and product slider in home page template

--- wp-content/themes/bootcommerce-5-child/page-home-without-container.php
+++ wp-content/themes/bootcommerce-5-child/page-home-without-container.php
@@ -18,6 +18,29 @@ get_header();
 			<div class="entry-content">
 				<?php the_post(); ?>
 				<?php the_content(); ?>
+
+				<div class="container">
+					<!-- <div class="container d-flex justify-content-center"> -->
+
+					<!-- slide articles -->
+					<?php
+					echo do_shortcode('[bs-post-slider type="post" category="non-classe" order="ASC" orderby="title" posts="8"]');
+					?>
+
+					<!-- slide products -->
+					<?php
+					echo do_shortcode('[bs-product-slider order="DESC" orderby="title" posts="12" category="ligne-w"]');
+					?>
+
+				</div>
+				<!-- video ?  -->
+				<div class="container-fluid text-center">
+					<a href="https://lignew.clients.arkima.io/configurator/app/index.html" target="_blank" rel="noopener noreferrer">
+						<iframe class="elementor-background-video-embed" frameborder="0" allowfullscreen="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" title="YouTube video player" width="640" height="360" src="https://www.youtube.com/embed/R8ljIK_ywz8?controls=0&amp;rel=0&amp;playsinline=1&amp;enablejsapi=1&amp;origin=http%3A%2F%2Flocalhost&amp;widgetid=1" id="widget2" style="width: 680px"></iframe>
+					</a>
+				</div>
+
+
 				<?php wp_link_pages(array(
 					'before' => '<div class="page-links">' . esc_html__('Pages:', 'bootscore'),
 					'after'  => '</div>',
