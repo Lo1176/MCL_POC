24720cbe628b3298bbf952d75b9d050dbb641043
Laurent Binder <laurentbinder@gmail.com>
2021-08-26 10:42:49 +0200

    custom.js getParameter function

--- wp-content/themes/bootcommerce-5-child/js/custom.js
+++ wp-content/themes/bootcommerce-5-child/js/custom.js
@@ -1,34 +1,35 @@
 jQuery(document).ready(function ($) {
-
-    // Do stuff here
-    // fonction pour récupérer les attributs des produits
-    $('#customize3d').on('click', function (){
-        // ajouter des if
-        var entretoise = $('#entretoise').val();
-        var couleur = $('#couleur').val();
-        var obj = {
-            entretoise: entretoise,
-            couleur: couleur
-        };
-        var url = $(this).attr('data-url');
-        $.post(url, obj, function (data) {
-            var url3d = data['url3d'];
-            window.location = url3d;
-            // alert(url3d);
-        });
+  // Do stuff here
+  // fonction pour récupérer les attributs des produits
+  $("#customize3d").on("click", function () {
+    // ajouter des if
+    var entretoise = $("#entretoise").val();
+    var couleur = $("#couleur").val();
+    var obj = {
+      entretoise: entretoise,
+      couleur: couleur,
+    };
+    var url = $(this).attr("data-url");
+    $.post(url, obj, function (data) {
+      var url3d = data["url3d"];
+      window.location = url3d;
+      // alert(url3d);
     });
+  });
 
-    // fonction pour récupérer les paramètres de l'URL
-    function getParameter(p) {
-      var url = window.location.search.substring(1); // récupère les paramètres
-      var varUrl = url.split("&"); // sépare les params quand il y a &
-      for (var i = 0; i < varUrl.length; i++) {
-        var parameter = varUrl[i].split("=");
-        if (parameter[0] == p) {
-          return parameter[1];
-          alert(parameter[1]);
-        }
+  // fonction pour récupérer les paramètres de l'URL
+  function getParameter(p) {
+    var url = window.location.search.substring(1); // récupère les paramètres
+    var varUrl = url.split("&"); // sépare les params autour de "&"
+    for (var i = 0; i < varUrl.length; i++) {
+      var parameter = varUrl[i].split("=");
+      if (parameter[0] == p) {
+        return parameter[1];
       }
-    //   getParameter(p);
     }
+    //   getParameter(p);
+    if (getParameter("entretoise")) {
+      var entretoise = getParameter("entretoise");
+    }
+  }
 }); // jQuery End
