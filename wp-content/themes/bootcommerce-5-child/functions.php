463c3d224d6c83c87627551edddcc3f81579cf18
Laurent Binder <laurentbinder@gmail.com>
2021-08-26 13:55:42 +0200

    js and function 3D api

--- wp-content/themes/bootcommerce-5-child/functions.php
+++ wp-content/themes/bootcommerce-5-child/functions.php
@@ -95,21 +95,21 @@ function my_3D_func($data)
 
     // test API avec l'EXUDE-API TBE
     $urlApi = "https://exude-api.herokuapp.com/exude/stopping/data";
-    // $apiTestRequest = array(
-    //     "links" => ["https://en.wikipedia.org/wiki/Rama"]
-    // );
-    // $apiTestRequestJSON = json_encode($apiTestRequest);
+    $apiTestRequest = array(
+        "links" => ["https://en.wikipedia.org/wiki/Rama"]
+    );
+    $apiTestRequestJSON = json_encode($apiTestRequest);
 
-    // wp_remote_post($urlApi, $apiTestRequestJSON);
+    wp_remote_post($urlApi, $apiTestRequestJSON);
 
-    // $apiTestRequest2 = array(
-    //     "data" => "Kannada is a Southern Dravidian language and according to scholar Sanford B. Steever, its history can be conventionally divided into three stages: Old Kannada (Halegannada) from 450–1200 AD, Middle Kannada (Nadugannada) from 1200–1700 and Modern Kannada from 1700 to the present.[23] Kannada is influenced to a considerable degree by Sanskrit. Influences of other languages such as Prakrit and Pali can also be found in Kannada. The scholar Iravatham Mahadevan indicated that Kannada was already a language of rich spoken tradition earlier than the 3rd century BC and based on the native Kannada words found in Prakrit inscriptions of that period, Kannada must have been spoken by a broad and stable population.[24][25] The scholar K. V. Narayana claims that many tribal languages which are now designated as Kannada dialects could be nearer to the earlier form of the language, with lesser influence from other languages"
-    // );
-    // $apiTestRequest2JSON = json_encode($apiTestRequest2);
-    // wp_remote_post($urlApi, $apiTestRequest2JSON);
+    $apiTestRequest2 = array(
+        "data" => "Kannada is a Southern Dravidian language and according to scholar Sanford B. Steever, its history can be conventionally divided into three stages: Old Kannada (Halegannada) from 450–1200 AD, Middle Kannada (Nadugannada) from 1200–1700 and Modern Kannada from 1700 to the present.[23] Kannada is influenced to a considerable degree by Sanskrit. Influences of other languages such as Prakrit and Pali can also be found in Kannada. The scholar Iravatham Mahadevan indicated that Kannada was already a language of rich spoken tradition earlier than the 3rd century BC and based on the native Kannada words found in Prakrit inscriptions of that period, Kannada must have been spoken by a broad and stable population.[24][25] The scholar K. V. Narayana claims that many tribal languages which are now designated as Kannada dialects could be nearer to the earlier form of the language, with lesser influence from other languages"
+    );
+    $apiTestRequest2JSON = json_encode($apiTestRequest2);
+    wp_remote_post($urlApi, $apiTestRequest2JSON);
 
     $options = [
-        'body'        => $api3dRequestJSON,
+        'body'        => $apiTestRequest2JSON,
         'headers'     => [
             'Content-Type' => 'application/json',
         ], // précise qu'on lui envoie du JSON en data
