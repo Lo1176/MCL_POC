11dec3fbf3c07365deed341809b6e5dd559ddcbf
Laurent Binder <laurentbinder@gmail.com>
2021-10-05 13:56:41 +0200

    rename header-bootscore-new and add css

--- wp-content/themes/bootcommerce-child-main/header-bootscore-new-04-oct.php
+++ wp-content/themes/bootcommerce-child-main/header-bootscore-new-04-oct.php
@@ -1,36 +1,37 @@
 <?php
-	/**
-	 * The header for our WooCommerce theme
-	 *
-	 * This is the template that displays all of the <head> section and everything up until <div id="content">
-	 *
-	 * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
-	 *
-	 * @package Bootscore
-	 */
-	
-	?>
+
+/**
+ * The header for our WooCommerce theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Bootscore
+ */
+
+?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
 
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="https://gmpg.org/xfn/11">
     <!-- Favicons -->
-    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_stylesheet_directory_uri();?>/img/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_stylesheet_directory_uri();?>/img/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_stylesheet_directory_uri();?>/img/favicon/favicon-16x16.png">
-    <link rel="manifest" href="<?php echo get_stylesheet_directory_uri();?>/img/favicon/site.webmanifest">
-    <link rel="mask-icon" href="<?php echo get_stylesheet_directory_uri();?>/img/favicon/safari-pinned-tab.svg" color="#0d6efd">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon/site.webmanifest">
+    <link rel="mask-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon/safari-pinned-tab.svg" color="#0d6efd">
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="theme-color" content="#ffffff">
-    <!-- Loads the internal WP jQuery. Required if a 3rd party plugin loads jQuery in header instead in footer -->
-    <?php wp_enqueue_script('jquery'); ?>
     <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
+
+    <?php wp_body_open(); ?>
 
     <div id="to-top"></div>
 
@@ -45,27 +46,27 @@
                     <div class="container">
 
                         <!-- Navbar Brand -->
-                        <a class="navbar-brand xs d-md-none" href="<?php echo esc_url( home_url() ); ?>"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/img/logo/logo-sm.svg" alt="logo" class="logo xs"></a>
-                        <a class="navbar-brand md d-none d-md-block" href="<?php echo esc_url( home_url() ); ?>"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/img/logo/logo.svg" alt="logo" class="logo md"></a>
+                        <a class="navbar-brand xs d-md-none" href="<?php echo esc_url(home_url()); ?>"><img src="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/img/logo/logo-sm.svg" alt="logo" class="logo xs"></a>
+                        <a class="navbar-brand md d-none d-md-block" href="<?php echo esc_url(home_url()); ?>"><img src="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/img/logo/logo.svg" alt="logo" class="logo md"></a>
 
                         <!-- Offcanvas Navbar -->
                         <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvas-navbar">
                             <div class="offcanvas-header bg-light">
-                                <h5 class="mb-0"><?php esc_html_e('Menu' , 'bootscore'); ?></h5>
+                                <span class="h5 mb-0"><?php esc_html_e('Menu', 'bootscore'); ?></span>
                                 <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                             </div>
                             <div class="offcanvas-body">
                                 <!-- Bootstrap 5 Nav Walker Main Menu -->
                                 <?php
-                                    wp_nav_menu(array(
-                                        'theme_location' => 'main-menu',
-                                        'container' => false,
-                                        'menu_class' => '',
-                                        'fallback_cb' => '__return_false',
-                                        'items_wrap' => '<ul id="bootscore-navbar" class="navbar-nav ms-auto %2$s">%3$s</ul>',
-                                        'depth' => 2,
-                                        'walker' => new bootstrap_5_wp_nav_menu_walker()
-                                    ));
+                                wp_nav_menu(array(
+                                    'theme_location' => 'main-menu',
+                                    'container' => false,
+                                    'menu_class' => '',
+                                    'fallback_cb' => '__return_false',
+                                    'items_wrap' => '<ul id="bootscore-navbar" class="navbar-nav ms-auto %2$s">%3$s</ul>',
+                                    'depth' => 2,
+                                    'walker' => new bootstrap_5_wp_nav_menu_walker()
+                                ));
                                 ?>
                                 <!-- Bootstrap 5 Nav Walker Main Menu End -->
                             </div>
@@ -75,10 +76,10 @@
 
                             <!-- Top Nav Widget -->
                             <div class="top-nav-widget">
-                                <?php if ( is_active_sidebar( 'top-nav' )) : ?>
-                                <div>
-                                    <?php dynamic_sidebar( 'top-nav' ); ?>
-                                </div>
+                                <?php if (is_active_sidebar('top-nav')) : ?>
+                                    <div>
+                                        <?php dynamic_sidebar('top-nav'); ?>
+                                    </div>
                                 <?php endif; ?>
                             </div>
 
@@ -95,15 +96,15 @@
                             <!-- Mini Cart Toggler -->
                             <button class="btn btn-outline-secondary ms-1 ms-md-2 position-relative" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvas-cart" aria-controls="offcanvas-cart">
                                 <i class="fas fa-shopping-bag"></i>
-                                <?php if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+                                <?php if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
                                     $count = WC()->cart->cart_contents_count;
                                 ?>
-                                <span class="cart-content">
-                                    <?php if ( $count > 0 ) { ?>
-                                    <?php echo esc_html( $count ); ?>
-                                    <?php
+                                    <span class="cart-content">
+                                        <?php if ($count > 0) { ?>
+                                            <?php echo esc_html($count); ?>
+                                        <?php
                                         }
-                                    ?></span>
+                                        ?></span>
                                 <?php } ?>
                             </button>
 
@@ -120,10 +121,10 @@
 
                 <!-- Top Nav Search Collapse -->
                 <div class="collapse container" id="collapse-search">
-                    <?php if ( is_active_sidebar( 'top-nav-search' )) : ?>
-                    <div class="mb-2">
-                        <?php dynamic_sidebar( 'top-nav-search' ); ?>
-                    </div>
+                    <?php if (is_active_sidebar('top-nav-search')) : ?>
+                        <div class="mb-2">
+                            <?php dynamic_sidebar('top-nav-search'); ?>
+                        </div>
                     <?php endif; ?>
                 </div>
 
@@ -132,7 +133,7 @@
             <!-- offcanvas user -->
             <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvas-user">
                 <div class="offcanvas-header bg-light">
-                    <h5 class="mb-0"><?php esc_html_e('Account' , 'bootscore'); ?></h5>
+                    <span class="h5 mb-0"><?php esc_html_e('Account', 'bootscore'); ?></span>
                     <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                 </div>
                 <div class="offcanvas-body">
@@ -145,7 +146,7 @@
             <!-- offcanvas cart -->
             <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvas-cart">
                 <div class="offcanvas-header bg-light">
-                    <h5 class="mb-0"><?php esc_html_e('Cart' , 'bootscore'); ?></h5>
+                    <span class="h5 mb-0"><?php esc_html_e('Cart', 'bootscore'); ?></span>
                     <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                 </div>
                 <div class="offcanvas-body p-0">
