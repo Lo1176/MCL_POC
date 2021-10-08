cd76bc6f6ac648a5ef484d1f9b255c2aa7d2b075
Laurent Binder <laurentbinder@gmail.com>
2021-10-08 10:44:51 +0200

    card-text characters lenght and no padding-x

--- wp-content/themes/bootcommerce-child/bs-swiper-main/sc-swiper-card.php
+++ wp-content/themes/bootcommerce-child/bs-swiper-main/sc-swiper-card.php
@@ -23,120 +23,131 @@ CPT Slider Shortcode
 
 
 // Card Slider Shortcode
-add_shortcode( 'bs-swiper-card', 'bootscore_swiper' );
-function bootscore_swiper( $atts ) {
-	ob_start();
-	extract( shortcode_atts( array (
-		'type' => 'post',
-		'order' => 'date',
-		'orderby' => 'date',
-		'posts' => -1,
-		'category' => '',
+add_shortcode('bs-swiper-card', 'bootscore_swiper');
+function bootscore_swiper($atts)
+{
+    ob_start();
+    extract(shortcode_atts(array(
+        'type' => 'post',
+        'order' => 'date',
+        'orderby' => 'date',
+        'posts' => -1,
+        'category' => '',
         'post_parent'    => '', // parent-id child-pages
         'cat_parent'    => '', // parent-taxonomy-id CPT
-		'tax' => '' // CPT taxonomy
-        
-	), $atts ) );
-	$options = array(
-		'post_type' => $type,
-		'order' => $order,
-		'orderby' => $orderby,
-		'posts_per_page' => $posts,
-		'category_name' => $category,
+        'tax' => '' // CPT taxonomy
+
+    ), $atts));
+    $options = array(
+        'post_type' => $type,
+        'order' => $order,
+        'orderby' => $orderby,
+        'posts_per_page' => $posts,
+        'category_name' => $category,
         'post_parent' => $post_parent,
-        
-	);
-    
+
+    );
+
     // CPT - Check if taxonomy and terms were defined
-	if ( $tax != '' && $cat_parent != '' ) {
-		$terms = explode( ',', trim( $cat_parent ) );
-		$terms = array_map( 'trim', $terms );
-		$terms = array_unique( $terms );
-		$terms = array_filter( $terms );
-		$options['tax_query'] = array(
-			'relation' => 'AND',
-			array(
-				'taxonomy' => $tax,
-				'field'    => 'term_id',
-				'terms'    => $terms,
-				'operator' => 'IN'
-			)
-		);
-	}
-    
-	$query = new WP_Query( $options );
-	if ( $query->have_posts() ) { ?>
+    if ($tax != '' && $cat_parent != '') {
+        $terms = explode(',', trim($cat_parent));
+        $terms = array_map('trim', $terms);
+        $terms = array_unique($terms);
+        $terms = array_filter($terms);
+        $options['tax_query'] = array(
+            'relation' => 'AND',
+            array(
+                'taxonomy' => $tax,
+                'field'    => 'term_id',
+                'terms'    => $terms,
+                'operator' => 'IN'
+            )
+        );
+    }
+
+    $query = new WP_Query($options);
+    if ($query->have_posts()) { ?>
 
 
-<!-- Swiper -->
+        <!-- Swiper -->
 
-<div class="px-5 position-relative">
+        <div class="px-5 position-relative">
 
-    <div class="cards swiper-container swiper position-static">
+            <div class="cards swiper-container swiper position-static">
 
-        <div class="swiper-wrapper">
+                <div class="swiper-wrapper">
 
-            <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+                    <?php while ($query->have_posts()) : $query->the_post(); ?>
 
-            <div class="swiper-slide card h-auto mb-5">
-                <!-- Featured Image-->
-                <?php the_post_thumbnail('medium', array('class' => 'card-img-top')); ?>
+                        <div class="swiper-slide card h-auto mb-5">
+                            <!-- Featured Image-->
+                            <?php the_post_thumbnail('medium', array('class' => 'card-img-top')); ?>
 
-                <div class="card-body d-flex flex-column">
+                            <div class="card-body d-flex flex-column px-0">
 
-                    <?php #bootscore_category_badge(); ?>
+                                <?php #bootscore_category_badge(); 
+                                ?>
 
-                    <!-- Title -->
-                    <h2 class="blog-post-title">
-                        <a href="<?php the_permalink(); ?>">
-                            <?php the_title(); ?>
-                        </a>
-                    </h2>
-                    <!-- Meta -->
-                    <?php if ( 'post' === get_post_type() ) : ?>
-                    <small class="text-muted mb-2">
-                        <?php
-                            bootscore_date();
-                            bootscore_author();
-                            #bootscore_comments();
-                            #bootscore_edit();
-                        ?>
-                    </small>
-                    <?php endif; ?>
-                    <!-- Excerpt & Read more -->
-                    <div class="card-text">
-                        <?php the_excerpt(); ?>
-                    </div>
+                                <!-- Title -->
+                                <h2 class="blog-post-title">
+                                    <a href="<?php the_permalink(); ?>">
+                                        <?php the_title(); ?>
+                                    </a>
+                                </h2>
+                                <!-- Meta -->
+                                <?php if ('post' === get_post_type()) : ?>
+                                    <small class="text-muted mb-2">
+                                        <?php
+                                        bootscore_date();
+                                        bootscore_author();
+                                        #bootscore_comments();
+                                        #bootscore_edit();
+                                        ?>
+                                    </small>
+                                <?php endif; ?>
+                                <!-- Excerpt & Read more -->
+                                <div class="">
+                                    <div class="card-text mb-3">
+                                        <?php
+                                        $excerpt = get_the_excerpt();
+                                        // disply only the first 200 characters
+                                        $excerpt = substr($excerpt, 0, 200);
+                                        $result = substr($excerpt, 0, strrpos($excerpt, ' '));
+                                        echo $result;
+                                        ?>
+                                    </div>
+                                    <div class="mt-auto text-start">
+                                        <a class="btn btn-primary read-more" href="<?php the_permalink(); ?>"><?php _e('Read more »', 'bootscore'); ?></a>
+                                    </div>
+                                </div>
 
-                    <div class="mt-auto">
-                        <a class="read-more" href="<?php the_permalink(); ?>"><?php _e('Read more »', 'bootscore'); ?></a>
-                    </div>
-                    <!-- Tags -->
-                    <?php bootscore_tags(); ?>
+                                <!-- Tags -->
+                                <?php bootscore_tags(); ?>
 
-                </div>
+                            </div>
 
-            </div><!-- .card -->
+                        </div><!-- .card -->
 
-            <?php endwhile; wp_reset_postdata(); ?>
+                    <?php endwhile;
+                    wp_reset_postdata(); ?>
 
-        </div> <!-- .swiper-wrapper -->
+                </div> <!-- .swiper-wrapper -->
 
-        <!-- Add Pagination -->
-        <div class="swiper-pagination"></div>
-        <!-- Add Arrows -->
-        <div class="swiper-button-next end-0"></div>
-        <div class="swiper-button-prev start-0"></div>
+                <!-- Add Pagination -->
+                <div class="swiper-pagination"></div>
+                <!-- Add Arrows -->
+                <div class="swiper-button-next end-0"></div>
+                <div class="swiper-button-prev start-0"></div>
 
-    </div><!-- swiper-container -->
+            </div><!-- swiper-container -->
 
-</div>
+        </div>
 
-<!-- Swiper End -->
+        <!-- Swiper End -->
 
 <?php $myvariable = ob_get_clean();
-	return $myvariable;
-	}	
+        return $myvariable;
+    }
 }
 
 // Card Slider Shortcode End
