2b2eb4bc1104cbf317fa614a2b0f3e95867651af
Laurent Binder <laurentbinder@gmail.com>
2021-10-14 15:11:36 +0200

    bootstrap col footer

--- wp-content/themes/bootcommerce-child/footer.php
+++ wp-content/themes/bootcommerce-child/footer.php
@@ -24,44 +24,51 @@
                 </div>
             <?php endif; ?>
 
-            <div class="row">
+            <div class="row bottom-footer">
 
-                <!-- Footer 1 Widget -->
-                <div class="col-md-6 col-lg-3">
-                    <?php if (is_active_sidebar('footer-1')) : ?>
-                        <div>
-                            <?php dynamic_sidebar('footer-1'); ?>
-                        </div>
-                    <?php endif; ?>
-                </div>
+                <!-- <div class="footer-left"> -->
+                    <!-- Footer 1 Widget -->
+                    <div class="col-md-6 col-lg-3">
+                    <!-- <div class="col"> -->
+                        <?php if (is_active_sidebar('footer-1')) : ?>
+                            <div>
+                                <?php dynamic_sidebar('footer-1'); ?>
+                            </div>
+                        <?php endif; ?>
+                    </div>
+                <!-- </div> .left END -->
 
-                <!-- Footer 2 Widget -->
-                <div class="col-md-6 col-lg-3">
-                    <?php if (is_active_sidebar('footer-2')) : ?>
-                        <div>
-                            <?php dynamic_sidebar('footer-2'); ?>
-                        </div>
-                    <?php endif; ?>
-                </div>
+                <!-- <div class="footer-right"> -->
 
-                <!-- Footer 3 Widget -->
-                <div class="col-md-6 col-lg-3">
-                    <?php if (is_active_sidebar('footer-3')) : ?>
-                        <div>
-                            <?php dynamic_sidebar('footer-3'); ?>
-                        </div>
-                    <?php endif; ?>
-                </div>
+                    <!-- Footer 2 Widget -->
+                    <div class="ms-auto col-md-6 col-lg-2">
+                        <?php if (is_active_sidebar('footer-2')) : ?>
+                            <div>
+                                <?php dynamic_sidebar('footer-2'); ?>
+                            </div>
+                        <?php endif; ?>
+                    </div>
 
-                <!-- Footer 4 Widget -->
-                <div class="col-md-6 col-lg-3">
-                    <?php if (is_active_sidebar('footer-4')) : ?>
-                        <div>
-                            <?php dynamic_sidebar('footer-4'); ?>
-                        </div>
-                    <?php endif; ?>
-                </div>
-                <!-- Footer Widgets End -->
+                    <!-- Footer 3 Widget -->
+                    <div class="col-md-6 col-lg-2">
+                        <?php if (is_active_sidebar('footer-3')) : ?>
+                            <div>
+                                <?php dynamic_sidebar('footer-3'); ?>
+                            </div>
+                        <?php endif; ?>
+                    </div>
+
+                    <!-- Footer 4 Widget -->
+                    <div class="col-md-6 col-lg-2">
+                        <?php if (is_active_sidebar('footer-4')) : ?>
+                            <div>
+                                <?php dynamic_sidebar('footer-4'); ?>
+                            </div>
+                        <?php endif; ?>
+                    </div>
+                    <!-- Footer Widgets End -->
+
+                <!-- </div> .right END -->
 
             </div>
 
@@ -78,7 +85,7 @@
             ));
             ?>
             <!-- Bootstrap 5 Nav Walker Footer Menu End -->
-            
+
         </div>
 
     </div>
