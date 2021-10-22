7b5b3ec40ef879125a0cde58359d48d02b985852
Laurent Binder <laurentbinder@gmail.com>
2021-10-22 16:00:43 +0200

    content in single.php

--- wp-content/themes/bootcommerce-child/single.php
+++ wp-content/themes/bootcommerce-child/single.php
@@ -1,61 +1,68 @@
 <?php
 /*
-	 * Template Name: post
 	 * Template Post Type: post
 	 */
 
 get_header();  ?>
-<div id="content" class="site-content container py-5 mt-4">
+
+<div id="content" class="site-content">
     <div id="primary" class="content-area">
-
-        <!-- Hook to add something nice -->
-        <?php bs_after_primary(); ?>
-
-        <?php the_breadcrumb(); ?>
 
         <main id="main" class="site-main">
 
-            <header class="entry-header">
-                <?php the_post(); ?>
-
-                <?php bootscore_category_badge(); ?>
-
-                <?php the_title('<h1>', '</h1>'); ?>
-                <p class="entry-meta">
-                    <small class="text-muted">
-                        <?php
-                        bootscore_date();
-                        _e(' by ', 'bootscore');
-                        the_author_posts_link();
-                        bootscore_comment_count();
-                        ?>
-                    </small>
-                </p>
-                <?php bootscore_post_thumbnail(); ?>
+            <?php the_post(); ?>
+            <?php $thumb = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full'); ?>
+            <header class="entry-header featured-full-width-img height-75 bg-dark text-light mb-3" style="background-image: url('<?php echo $thumb['0']; ?>')">
+                <div class="container entry-header h-100 d-flex align-items-end pb-3">
+                    <h1 class="entry-title"><?php the_title(); ?></h1>
+                </div>
             </header>
 
-            <div class="entry-content">
-                <?php the_content(); ?>
-            </div>
+            <div class="container pb-5">
 
-            <footer class="entry-footer clear-both">
-                <div class="mb-4">
-                    <?php bootscore_tags(); ?>
+                <!-- Hook to add something nice -->
+                <?php bs_after_primary(); ?>
+
+                <?php the_breadcrumb(); ?>
+
+                <div class="entry-content">
+
+                    <?php bootscore_category_badge(); ?>
+
+                    <p class="entry-meta">
+                        <small class="text-muted">
+                            <?php
+                            bootscore_date();
+                            _e(' by ', 'bootscore');
+                            the_author_posts_link();
+                            bootscore_comment_count();
+                            ?>
+                        </small>
+                    </p>
+
+                    <?php the_content(); ?>
+
                 </div>
-                <nav aria-label="Page navigation example">
-                    <ul class="pagination justify-content-center">
-                        <li class="page-item">
-                            <?php previous_post_link('%link'); ?>
-                        </li>
-                        <li class="page-item">
-                            <?php next_post_link('%link'); ?>
-                        </li>
-                    </ul>
-                </nav>
 
-            </footer>
+                <footer class="entry-footer clear-both">
+                    <div class="mb-4">
+                        <?php bootscore_tags(); ?>
+                    </div>
+                    <nav aria-label="Page navigation example">
+                        <ul class="pagination justify-content-center">
+                            <li class="page-item">
+                                <?php previous_post_link('%link'); ?>
+                            </li>
+                            <li class="page-item">
+                                <?php next_post_link('%link'); ?>
+                            </li>
+                        </ul>
+                    </nav>
+                </footer>
 
-            <?php comments_template(); ?>
+                <?php comments_template(); ?>
+
+            </div><!-- container -->
 
         </main><!-- #main -->
 
