5cc3747e1faef5c2f6456d21cadf4fe54f6875ba
Laurent Binder <laurentbinder@gmail.com>
2021-11-15 09:23:08 +0100

    updated WooCommerce Payments

--- wp-content/plugins/woocommerce-payments/vendor/jetpack-autoloader/class-version-loader.php
+++ wp-content/plugins/woocommerce-payments/vendor/jetpack-autoloader/class-version-loader.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jpcacea74f2a41fbca8d2a83250d17c4a7;
+namespace Automattic\Jetpack\Autoloader\jp522a778cab728fd24a7865ae23985279;
 
  // phpcs:ignore
 
