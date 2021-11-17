67caeb820d839d8d11476c81ccc4b65fb091f501
Laurent Binder <laurentbinder@gmail.com>
2021-11-17 12:02:39 +0100

    changed the css of the last word with .last-word or h1 > span:last-child and JS

--- wp-content/themes/bootcommerce-child/js/custom.js
+++ wp-content/themes/bootcommerce-child/js/custom.js
@@ -1,5 +1,20 @@
 jQuery(document).ready(function ($) {
   // Do stuff here
+  $(document).ready(function() {
+      $("div p").each(function (index, element) {
+        var heading = $(element);
+        var Word_array, last_Word, first_part;
 
+        Word_array = heading.html().split(/\s+/); // split on spaces
+        last_Word = Word_array.pop(); // pop the last Word
+        first_part = Word_array.join(" "); // rejoin the first words together
+
+        heading.html(
+          [first_part, ' <span class="last-word">', last_Word, "</span>"].join(
+            ""
+          )
+        );
+      });
+  });
 
 }); // jQuery End
