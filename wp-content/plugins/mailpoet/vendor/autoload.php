07877b1322c77d7e7432a15b7765be1fc4b2d0df
Laurent Binder <laurentbinder@gmail.com>
2021-11-20 22:57:09 +0100

    update MailPoet

--- wp-content/plugins/mailpoet/vendor/autoload.php
+++ wp-content/plugins/mailpoet/vendor/autoload.php
@@ -1,2 +1,2 @@
 <?php
- if (!defined('ABSPATH')) exit; require_once __DIR__ . '/composer/autoload_real.php'; return ComposerAutoloaderInit6edb8bdbca13b4824fa66c699ec9b4c7::getLoader(); 
+ if (!defined('ABSPATH')) exit; require_once __DIR__ . '/composer/autoload_real.php'; return ComposerAutoloaderInit712584b45755ff65b019c263842e1a6c::getLoader(); 
