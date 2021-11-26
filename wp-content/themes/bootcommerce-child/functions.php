e718dc785b813f762ea658b27f3dfef16b6f5b8c
Laurent Binder <laurentbinder@gmail.com>
2021-11-26 10:07:06 +0100

    function to hide product variable prices

--- wp-content/themes/bootcommerce-child/functions.php
+++ wp-content/themes/bootcommerce-child/functions.php
@@ -267,6 +267,44 @@ function my_3D_func($data)
   );
   return $api3dResponse;
 }
+/** test API END */
+
+//Hide Price Range for WooCommerce Variable Products
+function wc_varb_price_range($wcv_price, $product)
+{
+
+  // $prefix = sprintf('%s: ', __('From', 'wcvp_range'));
+  $prefix = sprintf('%s:', __('From', 'wcvp_range'));
+
+  $wcv_reg_min_price = $product->get_variation_regular_price('min', true);
+  $wcv_min_sale_price    = $product->get_variation_sale_price('min', true);
+  $wcv_max_price = $product->get_variation_price('max', true);
+  $wcv_min_price = $product->get_variation_price('min', true);
+
+  $wcv_price = ($wcv_min_sale_price == $wcv_reg_min_price) ?
+    wc_price($wcv_reg_min_price) :
+    '<del>' . wc_price($wcv_reg_min_price) . '</del>' . '<ins>' . wc_price($wcv_min_sale_price) . '</ins>';
+
+  return ($wcv_min_price == $wcv_max_price) ?
+    $wcv_price :
+    sprintf('%s%s', $prefix, $wcv_price);
+}
+
+add_filter('woocommerce_variable_sale_price_html', 'wc_varb_price_range', 10, 2);
+add_filter('woocommerce_variable_price_html', 'wc_varb_price_range', 10, 2);
+
+//Hide "From:$X" 
+add_filter('woocommerce_get_price_html', 'lw_hide_variation_price', 10, 2);
+function lw_hide_variation_price($v_price, $v_product)
+{
+  $v_product_types = array('variable');
+  if (in_array($v_product->get_type(), $v_product_types)) {
+    return '';
+  }
+  // return regular price 
+  return $v_price;
+}
+
 ##### end - CONTENT-SINGLE-PRODUCT #####
 
 
