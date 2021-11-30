ff21c5d88b44806fda38dbaad28a443247fd7946
Laurent Binder <laurentbinder@gmail.com>
2021-11-30 10:42:19 +0100

    changed btn-Qty with arrows left and right and JS

--- wp-content/themes/bootcommerce-child/woocommerce/global/quantity-input.php
+++ wp-content/themes/bootcommerce-child/woocommerce/global/quantity-input.php
@@ -31,7 +31,19 @@ if ($max_value && $min_value === $max_value) {
   <div class="quantity">
     <?php do_action('woocommerce_before_quantity_input_field'); ?>
     <label class="screen-reader-text" for="<?php echo esc_attr($input_id); ?>"><?php echo esc_attr($label); ?></label>
-    <input type="number" id="<?php echo esc_attr($input_id); ?>" class="input-text qty text form-control ms-auto ms-lg-0" step="<?php echo esc_attr($step); ?>" min="<?php echo esc_attr($min_value); ?>" max="<?php echo esc_attr(0 < $max_value ? $max_value : ''); ?>" name="<?php echo esc_attr($input_name); ?>" value="<?php echo esc_attr($input_value); ?>" title="<?php echo esc_attr_x('Qty', 'Product quantity input tooltip', 'woocommerce'); ?>" size="4" placeholder="<?php echo esc_attr($placeholder); ?>" inputmode="<?php echo esc_attr($inputmode); ?>" />
+    <div class="input-group inline-group">
+      <div class="input-group-prepend">
+        <button class="btn btn-outline-secondary btn-minus">
+          <i class="fas fa-angle-left"></i>
+        </button>
+      </div>
+      <input type="number" id="<?php echo esc_attr($input_id); ?>" class="input-text qty text form-control ms-auto ms-lg-0" step="<?php echo esc_attr($step); ?>" min="<?php echo esc_attr($min_value); ?>" max="<?php echo esc_attr(0 < $max_value ? $max_value : ''); ?>" name="<?php echo esc_attr($input_name); ?>" value="<?php echo esc_attr($input_value); ?>" title="<?php echo esc_attr_x('Qty', 'Product quantity input tooltip', 'woocommerce'); ?>" size="4" placeholder="<?php echo esc_attr($placeholder); ?>" inputmode="<?php echo esc_attr($inputmode); ?>" />
+      <div class="input-group-append">
+        <button class="btn btn-outline-secondary btn-plus">
+          <i class="fas fa-angle-right"></i>
+        </button>
+      </div>
+    </div>
     <?php do_action('woocommerce_after_quantity_input_field'); ?>
   </div>
 <?php
