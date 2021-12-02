dbf682fb056f41879905a9898e9d976329f33d28
Laurent Binder <laurentbinder@gmail.com>
2021-12-02 14:55:33 +0100

    repsponsive .product-quantity and .quantity btn

--- wp-content/themes/bootcommerce-child/woocommerce/global/quantity-input.php
+++ wp-content/themes/bootcommerce-child/woocommerce/global/quantity-input.php
@@ -28,11 +28,11 @@ if ($max_value && $min_value === $max_value) {
   /* translators: %s: Quantity. */
   $label = !empty($args['product_name']) ? sprintf(esc_html__('%s quantity', 'woocommerce'), wp_strip_all_tags($args['product_name'])) : esc_html__('Quantity', 'woocommerce');
 ?>
-  <div class="quantity input-group me-3 w-50">
-    <button type="button" class="input-group-text btn qty_button minus"><</button>
+  <div class="quantity input-group col">
+    <button type="button" class="input-group-text qty_button minus"><</button>
     <label class="screen-reader-text" for="<?php echo esc_attr($input_id); ?>"><?php esc_html_e('Quantity', 'woocommerce'); ?></label>
     <input type="number" id="<?php echo esc_attr($input_id); ?>" class="input-text qty text form-control" step="<?php echo esc_attr($step); ?>" min="<?php echo esc_attr($min_value); ?>" max="<?php echo esc_attr(0 < $max_value ? $max_value : ''); ?>" name="<?php echo esc_attr($input_name); ?>" value="<?php echo esc_attr($input_value); ?>" title="<?php echo esc_attr_x('Qty', 'Product quantity input tooltip', 'woocommerce'); ?>" size="4" pattern="<?php echo esc_attr($pattern); ?>" inputmode="<?php echo esc_attr($inputmode); ?>" aria-labelledby="<?php echo esc_attr($labelledby); ?>" />
-    <button type="button" class="input-group-text btn qty_button plus">></button>
+    <button type="button" class="input-group-text qty_button plus">></button>
   </div>
 <?php
 }
--- wp-content/themes/bootcommerce-child/woocommerce/single-product/add-to-cart/simple.php
+++ wp-content/themes/bootcommerce-child/woocommerce/single-product/add-to-cart/simple.php
@@ -30,7 +30,7 @@ if ($product->is_in_stock()) : ?>
 
   <?php do_action('woocommerce_before_add_to_cart_form'); ?>
 
-  <form class="d-flex align-items-center justify-content-end col-sm cart" action="<?php echo esc_url(apply_filters('woocommerce_add_to_cart_form_action', $product->get_permalink())); ?>" method="post" enctype='multipart/form-data'>
+  <form class="d-flex align-items-center justify-content-end cart col-sm-6 offset-sm-6" action="<?php echo esc_url(apply_filters('woocommerce_add_to_cart_form_action', $product->get_permalink())); ?>" method="post" enctype='multipart/form-data'>
     <?php do_action('woocommerce_before_add_to_cart_button'); ?>
 
     <?php
@@ -45,7 +45,7 @@ if ($product->is_in_stock()) : ?>
     do_action('woocommerce_after_add_to_cart_quantity');
     ?>
 
-    <button type="submit" name="add-to-cart" value="<?php echo esc_attr($product->get_id()); ?>" class="single_add_to_cart_button alt btn btn-primary"><?php echo esc_html($product->single_add_to_cart_text()); ?></button>
+    <button type="submit" name="add-to-cart" value="<?php echo esc_attr($product->get_id()); ?>" class="single_add_to_cart_button alt btn btn-primary col-sm-7"><?php echo esc_html($product->single_add_to_cart_text()); ?></button>
 
     <?php do_action('woocommerce_after_add_to_cart_button'); ?>
   </form>
