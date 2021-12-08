68ca08ea5f6c176027256a3948cef89633e00136
Laurent Binder <laurentbinder@gmail.com>
2021-12-08 10:14:27 +0100

    responsive gty-btn css

--- wp-content/themes/bootcommerce-child/woocommerce/single-product/add-to-cart/simple.php
+++ wp-content/themes/bootcommerce-child/woocommerce/single-product/add-to-cart/simple.php
@@ -45,7 +45,7 @@ if ($product->is_in_stock()) : ?>
     do_action('woocommerce_after_add_to_cart_quantity');
     ?>
 
-    <button type="submit" name="add-to-cart" value="<?php echo esc_attr($product->get_id()); ?>" class="single_add_to_cart_button alt btn btn-primary col-sm-7"><?php echo esc_html($product->single_add_to_cart_text()); ?></button>
+    <button type="submit" name="add-to-cart" value="<?php echo esc_attr($product->get_id()); ?>" class="single_add_to_cart_button alt btn btn-primary col-6"><?php echo esc_html($product->single_add_to_cart_text()); ?></button>
 
     <?php do_action('woocommerce_after_add_to_cart_button'); ?>
   </form>
