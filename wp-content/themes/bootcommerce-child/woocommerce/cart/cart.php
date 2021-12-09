6af0695cc7a300b80eeb97ca0e1a71b70a5f8815
Laurent Binder <laurentbinder@gmail.com>
2021-12-09 12:57:05 +0100

    test wishlist in cart.php but it won't shows

--- wp-content/themes/bootcommerce-child/woocommerce/cart/cart.php
+++ wp-content/themes/bootcommerce-child/woocommerce/cart/cart.php
@@ -22,7 +22,7 @@ do_action('woocommerce_before_cart'); ?>
 
 <form class="woocommerce-cart-form" action="<?php echo esc_url(wc_get_cart_url()); ?>" method="post">
   <?php do_action('woocommerce_before_cart_table'); ?>
-
+  
   <table class="shop_table shop_table_responsive cart woocommerce-cart-form__contents rounded" cellspacing="0">
     <thead>
       <tr>
@@ -36,18 +36,25 @@ do_action('woocommerce_before_cart'); ?>
     </thead>
     <tbody>
       <?php do_action('woocommerce_before_cart_contents'); ?>
-
+      
       <?php
       foreach (WC()->cart->get_cart() as $cart_item_key => $cart_item) {
         $_product   = apply_filters('woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key);
         $product_id = apply_filters('woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key);
-
+        
         if ($_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters('woocommerce_cart_item_visible', true, $cart_item, $cart_item_key)) {
           $product_permalink = apply_filters('woocommerce_cart_item_permalink', $_product->is_visible() ? $_product->get_permalink($cart_item) : '', $cart_item, $cart_item_key);
-      ?>
+          ?>
           <tr class="woocommerce-cart-form__cart-item <?php echo esc_attr(apply_filters('woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key)); ?>">
-
+            
             <td class="product-remove">
+              <!-- test wishlist ici  -->
+              <div class="bg-warning">
+                WISH HERE WON'T SHOWS
+                <?php echo do_shortcode('[ti_wishlists_addtowishlist loop=yes]'); ?>
+              </div>
+              <!-- test wishlist ici  -->
+
               <?php
               echo apply_filters( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
                 'woocommerce_cart_item_remove_link',
@@ -62,11 +69,11 @@ do_action('woocommerce_before_cart'); ?>
               );
               ?>
             </td>
-
+            
             <td class="product-thumbnail">
               <?php
               $thumbnail = apply_filters('woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key);
-
+              
               if (!$product_permalink) {
                 echo $thumbnail; // PHPCS: XSS ok.
               } else {
@@ -74,7 +81,7 @@ do_action('woocommerce_before_cart'); ?>
               }
               ?>
             </td>
-
+            
             <td class="product-name" data-title="<?php esc_attr_e('Product', 'woocommerce'); ?>">
               <?php
               if (!$product_permalink) {
@@ -82,25 +89,25 @@ do_action('woocommerce_before_cart'); ?>
               } else {
                 echo wp_kses_post(apply_filters('woocommerce_cart_item_name', sprintf('<a href="%s">%s</a>', esc_url($product_permalink), esc_html($_product->get_name())), $cart_item, $cart_item_key));
               }
-
+              
               do_action('woocommerce_after_cart_item_name', $cart_item, $cart_item_key);
-
+              
               // Meta data.
               echo wc_get_formatted_cart_item_data($cart_item); // PHPCS: XSS ok.
-
+              
               // Backorder notification.
               if ($_product->backorders_require_notification() && $_product->is_on_backorder($cart_item['quantity'])) {
                 echo wp_kses_post(apply_filters('woocommerce_cart_item_backorder_notification', '<p class="backorder_notification">' . esc_html__('Available on backorder', 'woocommerce') . '</p>', $product_id));
               }
               ?>
             </td>
-
+            
             <td class="product-price" data-title="<?php esc_attr_e('Price', 'woocommerce'); ?>">
               <?php
               echo apply_filters('woocommerce_cart_item_price', WC()->cart->get_product_price($_product), $cart_item, $cart_item_key); // PHPCS: XSS ok.
               ?>
             </td>
-
+            
             <td class="product-quantity" data-title="<?php esc_attr_e('Quantity', 'woocommerce'); ?>">
               <?php
               if ($_product->is_sold_individually()) {
@@ -118,18 +125,18 @@ do_action('woocommerce_before_cart'); ?>
                   false
                 );
               }
-
+              
               echo apply_filters('woocommerce_cart_item_quantity', $product_quantity, $cart_item_key, $cart_item); // PHPCS: XSS ok.
               ?>
             </td>
-
+            
             <td class="product-subtotal" data-title="<?php esc_attr_e('Subtotal', 'woocommerce'); ?>">
               <?php
               echo apply_filters('woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal($_product, $cart_item['quantity']), $cart_item, $cart_item_key); // PHPCS: XSS ok.
               ?>
             </td>
           </tr>
-      <?php
+          <?php
         }
       }
       ?>
