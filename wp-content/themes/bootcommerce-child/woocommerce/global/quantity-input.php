2370d47e5e1284427bac54ed5d70f73c2cd740d5
Laurent Binder <laurentbinder@gmail.com>
2021-12-15 15:44:54 +0100

    add arrow design by Gilles

--- wp-content/themes/bootcommerce-child/woocommerce/global/quantity-input.php
+++ wp-content/themes/bootcommerce-child/woocommerce/global/quantity-input.php
@@ -30,10 +30,10 @@ if ($max_value && $min_value === $max_value) {
 ?>
   <!-- <div class="quantity input-group "> -->
   <div class="quantity button-arrow-qty justify-content-center col-sm-6">
-    <button type="button" class="input-group-text qty_button minus"><</button>
+    <button type="button" class="input-group-text qty_button minus"><img src="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/img/svg/arrow-l.svg" alt="smaller"></button>
     <label class="screen-reader-text" for="<?php echo esc_attr($input_id); ?>"><?php esc_html_e('Quantity', 'woocommerce'); ?></label>
     <input type="number" id="<?php echo esc_attr($input_id); ?>" class="input-text qty text form-control" step="<?php echo esc_attr($step); ?>" min="<?php echo esc_attr($min_value); ?>" max="<?php echo esc_attr(0 < $max_value ? $max_value : ''); ?>" name="<?php echo esc_attr($input_name); ?>" value="<?php echo esc_attr($input_value); ?>" title="<?php echo esc_attr_x('Qty', 'Product quantity input tooltip', 'woocommerce'); ?>" size="4" pattern="<?php echo esc_attr($pattern); ?>" inputmode="<?php echo esc_attr($inputmode); ?>" aria-labelledby="<?php echo esc_attr($labelledby); ?>" />
-    <button type="button" class="input-group-text qty_button plus">></button>
+    <button type="button" class="input-group-text qty_button plus"><img src="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/img/svg/arrow-r.svg" alt="higher"></button>
   </div>
 <?php
 }
