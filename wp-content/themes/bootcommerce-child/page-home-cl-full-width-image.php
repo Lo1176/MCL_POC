b1a89747b5a2380cdc919864aec742b11ebbc58a
Laurent Binder <laurentbinder@gmail.com>
2021-12-21 14:02:22 +0100

    sidebar-1 no padding-botttom

--- wp-content/themes/bootcommerce-child/page-home-cl-full-width-image.php
+++ wp-content/themes/bootcommerce-child/page-home-cl-full-width-image.php
@@ -21,126 +21,123 @@ get_header();
         <div class="container entry-header d-flex justify-content-center pb-3">
           <h1 id="brand-logo-header" class="d-flex entry-title"><img id="cl-logo" src=<?php echo do_shortcode('[homeurl]/wp-content/uploads/2021/10/cl-uncolored.svg'); ?> alt="Maison Château Laguiole image">
         </div>
-        <!-- no title tbe -->
-        <!-- no title tbe END -->
       </header>
 
-      <div class="container-fluid pb-5">
-        <!-- test LEFT SEARCH  -->
-        <div class="container-fluid col-2">
 
-        </div>
-        <!-- END SEARCH -->
+      <div class="container-fluid pb-5">
         <!-- Hook to add something nice -->
         <?php bs_after_primary(); ?>
         <!-- nothing in there for the moment , change it in function.php -->
 
         <div class="entry-content container">
-          <!-- all the content for wordpress 'modifier la page' -->
+          <!-- all the content from wordpress 'modifier la page' -->
           <?php the_content(); ?>
-          <!-- all the content for wordpress 'modifier la page' END -->
+          <!-- all the content from wordpress 'modifier la page' END -->
         </div>
         <!-- end content from WP  -->
+        <div class="row">
+          <!-- sidebar -->
+          <?php get_sidebar(); ?>
+          <!-- collection from Chateau Laguiole -->
+          <div class="container-fluid d-flex justify-content-center col-10 col-sm-10">
+            <div id="collection" class="d-flex justify-content-center flex-wrap">
 
-        <!-- collection from Chateau Laguiole -->
-        <div class="container-fluid d-flex justify-content-center">
-          <div id="collection" class="d-flex justify-content-center flex-wrap col-12 col-sm-10">
+              <!-- ##### ecrit en DUR !!!!  ##### -->
+              <div class="card m-2" style="width: 18rem;">
+                <a href="<?php echo do_shortcode('[homeurl]/categorie-produit/chateau-laguiole/classique/'); ?>">
+                  <img class="card-img-top" src="<?php echo do_shortcode('[homeurl]/wp-content/uploads/2021/07/CL-classique-bois-erable-scaled.jpeg'); ?>" alt="Produc image cap">
+                  <div class="card-body">
+                    <h5 class="card-title"><strong>Classique</strong></h5>
+                    <p class="card-text">Some quick example text</p>
+                  </div>
+                </a>
+              </div>
 
-            <!-- ##### ecrit en DUR !!!!  ##### -->
-            <div class="card m-2" style="width: 18rem;">
-              <a href="<?php echo do_shortcode('[homeurl]/categorie-produit/chateau-laguiole/classique/'); ?>">
-                <img class="card-img-top" src="<?php echo do_shortcode('[homeurl]/wp-content/uploads/2021/07/CL-classique-bois-erable-scaled.jpeg'); ?>" alt="Produc image cap">
-                <div class="card-body">
-                  <h5 class="card-title"><strong>Classique</strong></h5>
-                  <p class="card-text">Some quick example text</p>
-                </div>
-              </a>
-            </div>
+              <div class="card m-2" style="width: 18rem;">
+                <a href="<?php echo do_shortcode('[homeurl]/categorie-produit/chateau-laguiole/grand-cru/'); ?>">
+                  <img class="card-img-top" src="<?php echo do_shortcode('[homeurl]/wp-content/uploads/2021/07/CL-Grand-Cru-bois-débène-entretoise-jaune--scaled.jpeg'); ?>" alt="Produc image cap">
+                  <div class="card-body">
+                    <h5 class="card-title"><strong>Grand Cru</strong></h5>
+                    <p class="card-text">Some quick example text</p>
+                  </div>
+                </a>
+              </div>
+              <div class="card m-2" style="width: 18rem;">
+                <a href="<?php echo do_shortcode('[homeurl]/categorie-produit/chateau-laguiole/versailles/'); ?>">
+                  <img class="card-img-top" src="<?php echo do_shortcode('[homeurl]/wp-content/uploads/2021/07/CL-Versailles-Genévrier-600x400.jpeg'); ?>" alt="Produc image cap">
+                  <div class="card-body">
+                    <h5 class="card-title"><strong>Versailles</strong></h5>
+                    <p class="card-text">Some quick example text</p>
+                  </div>
+                </a>
+              </div>
+              <div class="card m-2" style="width: 18rem;">
+                <a href="<?php echo do_shortcode('[homeurl]/categorie-produit/chateau-laguiole/meilleur-sommelier/'); ?>">
+                  <img class="card-img-top" src="<?php echo do_shortcode('[homeurl]/wp-content/uploads/2021/07/CLME3033Enrico-Bernardo--scaled.jpeg'); ?>" alt="Produc image cap">
+                  <div class="card-body">
+                    <h5 class="card-title"><strong>Meilleur Sommelier</strong></h5>
+                    <p class="card-text">Some quick example text</p>
+                  </div>
+                </a>
+              </div>
+              <div class="card m-2" style="width: 18rem;">
+                <a href="<?php #echo do_shortcode('[homeurl]/categorie-produit/chateau-laguiole/opus/'); 
+                          ?>">
+                  <img class="card-img-top" src="<?php echo do_shortcode('[homeurl]/wp-content/uploads/2021/07/CLSOG0201-Grand-Cru-Opus-N°-2-Cocobolo-wood-scaled.jpeg'); ?>" alt="Produc image cap">
+                  <div class="card-body">
+                    <h5 class="card-title"><strong>Opus</strong></h5>
+                    <p class="card-text">Some quick example text</p>
+                  </div>
+                </a>
+              </div>
+              <div class="card m-2" style="width: 18rem;">
+                <a href="<?php #echo do_shortcode('[homeurl]/categorie-produit/chateau-laguiole/melchior/'); 
+                          ?>">
+                  <img class="card-img-top" src="<?php echo do_shortcode('[homeurl]/wp-content/uploads/2021/07/CL-melchior-ironwood.jpeg'); ?>" alt="Produc image cap">
+                  <div class="card-body">
+                    <h5 class="card-title"><strong>Melchior</strong></h5>
+                    <p class="card-text">Some quick example text</p>
+                  </div>
+                </a>
+              </div>
+              <div class="card m-2" style="width: 18rem;">
+                <a href="<?php echo do_shortcode('[homeurl]/chateau-laguiole/couteaux-pliants/'); ?>">
+                  <img class="card-img-top" src="<?php echo do_shortcode('[homeurl]/wp-content/uploads/2021/07/COPGC7102-Corne-blonde-White-horn-1-scaled.jpg'); ?>" alt="Produc image cap">
+                  <div class="card-body">
+                    <h5 class="card-title"><strong>Couteaux Pliant</strong></h5>
+                    <p class="card-text">Some quick example text</p>
+                  </div>
+                </a>
+              </div>
+              <div class="card m-2" style="width: 18rem;">
+                <a href="<?php #echo do_shortcode('[homeurl]/chateau-laguiole/couteaux-de-table/'); 
+                          ?>">
+                  <img class="card-img-top" src="<?php echo do_shortcode('[homeurl]/wp-content/uploads/2021/07/chateau-laguiole-steak-knives-brazilian-rosewood-set-of-6-chateau-laguioler-made-in-france_5000x.jpg'); ?>" alt="Produc image cap">
+                  <div class="card-body">
+                    <h5 class="card-title"><strong>Couteaux de table</strong></h5>
+                    <p class="card-text">Some quick example text</p>
+                  </div>
+                </a>
+              </div>
 
-            <div class="card m-2" style="width: 18rem;">
-              <a href="<?php echo do_shortcode('[homeurl]/categorie-produit/chateau-laguiole/grand-cru/'); ?>">
-                <img class="card-img-top" src="<?php echo do_shortcode('[homeurl]/wp-content/uploads/2021/07/CL-Grand-Cru-bois-débène-entretoise-jaune--scaled.jpeg'); ?>" alt="Produc image cap">
-                <div class="card-body">
-                  <h5 class="card-title"><strong>Grand Cru</strong></h5>
-                  <p class="card-text">Some quick example text</p>
-                </div>
-              </a>
-            </div>
-            <div class="card m-2" style="width: 18rem;">
-              <a href="<?php echo do_shortcode('[homeurl]/categorie-produit/chateau-laguiole/versailles/'); ?>">
-                <img class="card-img-top" src="<?php echo do_shortcode('[homeurl]/wp-content/uploads/2021/07/CL-Versailles-Genévrier-600x400.jpeg'); ?>" alt="Produc image cap">
-                <div class="card-body">
-                  <h5 class="card-title"><strong>Versailles</strong></h5>
-                  <p class="card-text">Some quick example text</p>
-                </div>
-              </a>
-            </div>
-            <div class="card m-2" style="width: 18rem;">
-              <a href="<?php echo do_shortcode('[homeurl]/categorie-produit/chateau-laguiole/meilleur-sommelier/'); ?>">
-                <img class="card-img-top" src="<?php echo do_shortcode('[homeurl]/wp-content/uploads/2021/07/CLME3033Enrico-Bernardo--scaled.jpeg'); ?>" alt="Produc image cap">
-                <div class="card-body">
-                  <h5 class="card-title"><strong>Meilleur Sommelier</strong></h5>
-                  <p class="card-text">Some quick example text</p>
-                </div>
-              </a>
-            </div>
-            <div class="card m-2" style="width: 18rem;">
-              <a href="<?php #echo do_shortcode('[homeurl]/categorie-produit/chateau-laguiole/opus/'); 
-                        ?>">
-                <img class="card-img-top" src="<?php echo do_shortcode('[homeurl]/wp-content/uploads/2021/07/CLSOG0201-Grand-Cru-Opus-N°-2-Cocobolo-wood-scaled.jpeg'); ?>" alt="Produc image cap">
-                <div class="card-body">
-                  <h5 class="card-title"><strong>Opus</strong></h5>
-                  <p class="card-text">Some quick example text</p>
-                </div>
-              </a>
-            </div>
-            <div class="card m-2" style="width: 18rem;">
-              <a href="<?php #echo do_shortcode('[homeurl]/categorie-produit/chateau-laguiole/melchior/'); 
-                        ?>">
-                <img class="card-img-top" src="<?php echo do_shortcode('[homeurl]/wp-content/uploads/2021/07/CL-melchior-ironwood.jpeg'); ?>" alt="Produc image cap">
-                <div class="card-body">
-                  <h5 class="card-title"><strong>Melchior</strong></h5>
-                  <p class="card-text">Some quick example text</p>
-                </div>
-              </a>
-            </div>
-            <div class="card m-2" style="width: 18rem;">
-              <a href="<?php echo do_shortcode('[homeurl]/chateau-laguiole/couteaux-pliants/'); ?>">
-                <img class="card-img-top" src="<?php echo do_shortcode('[homeurl]/wp-content/uploads/2021/07/COPGC7102-Corne-blonde-White-horn-1-scaled.jpg'); ?>" alt="Produc image cap">
-                <div class="card-body">
-                  <h5 class="card-title"><strong>Couteaux Pliant</strong></h5>
-                  <p class="card-text">Some quick example text</p>
-                </div>
-              </a>
-            </div>
-            <div class="card m-2" style="width: 18rem;">
-              <a href="<?php #echo do_shortcode('[homeurl]/chateau-laguiole/couteaux-de-table/'); 
-                        ?>">
-                <img class="card-img-top" src="<?php echo do_shortcode('[homeurl]/wp-content/uploads/2021/07/chateau-laguiole-steak-knives-brazilian-rosewood-set-of-6-chateau-laguioler-made-in-france_5000x.jpg'); ?>" alt="Produc image cap">
-                <div class="card-body">
-                  <h5 class="card-title"><strong>Couteaux de table</strong></h5>
-                  <p class="card-text">Some quick example text</p>
-                </div>
-              </a>
             </div>
 
           </div>
-
         </div>
-      </div>
 
 
-      <footer class="entry-footer">
 
-      </footer>
+        <footer class="entry-footer">
 
-      <?php comments_template(); ?>
+        </footer>
 
-  </div><!-- container -->
+        <?php comments_template(); ?>
 
-  </main><!-- #main -->
+      </div><!-- container-fluid -->
 
-</div><!-- #primary -->
+    </main><!-- #main -->
+
+  </div><!-- #primary -->
 </div><!-- #content -->
 <?php
 get_footer();
