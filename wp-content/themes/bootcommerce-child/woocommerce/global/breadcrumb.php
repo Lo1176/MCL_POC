b227b022295190e028e9ee8cbdf7d4f366bfe8ee
Laurent Binder <laurentbinder@gmail.com>
2022-01-14 16:47:10 +0100

    add link and MCL in breadcrumb

--- wp-content/themes/bootcommerce-child/woocommerce/global/breadcrumb.php
+++ wp-content/themes/bootcommerce-child/woocommerce/global/breadcrumb.php
@@ -32,7 +32,7 @@ if (!empty($breadcrumb)) {
     if (!empty($crumb[1]) && sizeof($breadcrumb) !== $key + 1) {
       if (0 == $key) {
         #echo '<a href="' . esc_url($crumb[1]) . '"><i class="fas fa-home"></i></a>';
-        echo '<a href="' . esc_url($crumb[1]) . '">' . bloginfo('title') . '</a>';
+        echo '<a href="' . home_url() . '">MAISON CHATEAU LAGUIOLE</a>';
       } else {
         echo '<a href="' . esc_url($crumb[1]) . '">' . esc_html($crumb[0]) . '</a>';
       }
